928afa306199437ede82ce5dff58394f30275484
MrPal28 <arindampal669@gmail.com>
2026-09-16 16:03:08 +0530

    Remove legacy key pair management and add public key file for Ansible setup

--- Terraform/key-pair.tf
+++ Terraform/key-pair.tf
@@ -1,15 +1,4 @@
-resource "tls_private_key" "ansible_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "local_file" "ansible_private_key" {
-  content         = tls_private_key.ansible_key.private_key_pem
-  filename        = "terraform-ansible-app.pem"
-  file_permission = "0600"
-}
-
 resource "aws_key_pair" "ansible_key" {
   key_name   = "terraform-ansible-app-key"
-  public_key = tls_private_key.ansible_key.public_key_openssh
+  public_key = file("${path.module}/keys/terraform-ansible-app-key.pub")
 }
