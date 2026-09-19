b776f41340d9ae329406807554897091633161b7
myselfAg <agneeshpersonal@gmail.com>
2026-09-19 14:13:44 +0530

    Fixed Backend

--- Terraform/remote-backend/terraform.tf
+++ Terraform/remote-backend/terraform.tf
@@ -6,3 +6,12 @@ terraform {
     }
   }
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucket-ap-am-2026"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-ansible-dynamodb-state-table"
+  }
+}
